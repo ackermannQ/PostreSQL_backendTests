f0c64c8ef6cad91cf69242e2c1840fc11998ec48
ackermannQ <ackermann.quentin@gmail.com>
2020-06-28 21:58:12 +0200

    PostgreSQL backend tests

---
+++ src/main/java/com/lunchz/LunchzApplication.java
@@ -0,0 +1,13 @@
+package com.lunchz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LunchzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LunchzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lunchz/LunchzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lunchz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LunchzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
